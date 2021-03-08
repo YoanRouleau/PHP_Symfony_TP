99e86b8eef8280f91453692e34405cde2ba2f8ba
YoanRouleau <yoan-rouleau@live.fr>
2021-03-08 10:48:35 +0100

    Easyadmin CRUD

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Symfony Tp');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
+            // Users
+            MenuItem::section('Users'),
+            MenuItem::linkToCrud('Users', 'fa fa-users', User::class),
+
+            // Posts
+            MenuItem::section('Posts'),
+            MenuItem::linkToCrud('Posts', 'fa fa-file-alt', Post::class),
+
+            // Comments
+            MenuItem::section('Comments'),
+            MenuItem::linkToCrud('Comments', 'fa fa-comments', Comment::class),
+
+        ];
+    }
+}
